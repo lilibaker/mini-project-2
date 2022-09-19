6bc47c5f49b18dca8d07babbdcaeb1d0742fab64
olivia <oliviachang29@gmail.com>
2022-09-19 15:28:39 -0400

    add panning

--- MiniProject2.ino
+++ MiniProject2.ino
@@ -1,41 +1,83 @@
 #include <Servo.h>
-//#include <SharpIR.h>
 
-Servo servo1;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-Servo servo2;
+// servo 1 = up and down
+Servo servoV;
+// servo 2 = left and right
+Servo servoH;
 
-int pos = 0;    // variable to store the servo position
+int servoV_pos = 0; // 0 to 180
+int servoH_pos = 0; // 0 to 180
 
-#define model 20150
-//const int SENSOR_TO_CM = 25.4 / 410;
-const int delay_const = 100;
+int servo_unit_of_distance = 18;
+
+const int SERVO_MAX = 180; // defined by servo/arduino
+
+// pin definitions
 const int sensorPin = A0;
 const int servoPin1 = 9;
 const int servoPin2 = 10;
-int distance = 0;
+const int buttonPin = 8;
 
-//SharpIR IR_prox(model, sensorPin);
+const int PAN_DELAY = 200;
+
+int distance = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  servo1.attach(servoPin1);
-  // servo2.attach(servoPin2);
+  Serial.println("Sketch starting...");
+  
+  // Set up servos
+  servoV.attach(servoPin1);
+  servoH.attach(servoPin2);
+
+  // Set up button pin
+  pinMode(buttonPin, INPUT);
+  
+  // Attach interrupt function that is called when button pin state goes from LOW to HIGH
+  attachInterrupt(digitalPinToInterrupt(buttonPin), onButtonPress, RISING);
+
+  resetServos();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
-  for (int x_pos = 0; x_pos <= 180; x_pos++) {
-    scan();
-    servo1.write(x_pos);
-    delay(delay_const);
+}
+
+void onButtonPress() {
+  resetServos();
+  Serial.println("Start scanning");
+  // for loop up and down
+  for (servoV_pos = 0; servoV_pos <= SERVO_MAX; servoV_pos = servoV_pos + servo_unit_of_distance) {
+    // for loop left and right
+    for (servoH_pos = 0; servoH_pos <= SERVO_MAX; servoH_pos = servoH_pos + servo_unit_of_distance) {
+      scan();
+      writeToServos();
+      delay(PAN_DELAY);
+    }
   }
+  Serial.println("Finished scanning");
+}
+
+void writeToServos() {
+  Serial.print("Current pos: x ");
+  Serial.print(servoH_pos);
+  Serial.print(", y ");
+  Serial.println(servoV_pos);
+  
+  servoV.write(servoV_pos);
+  servoH.write(servoH_pos);
+}
+
+void resetServos() {
+  Serial.println("Reset servos");
+  servoV_pos = 0;
+  servoH_pos = 0;
+  writeToServos();
 }
 
 void scan(){
-  distance = analogRead(sensorPin);
-  float new_distance = 29.988 * pow(distance, -1.173) * 100; // in cm
-  Serial.print(new_distance);
+  float rawDistance = analogRead(sensorPin);
+  float convertedDistance = 29.988 * pow(rawDistance, -1.173) * 100; // in cm
+  Serial.println(convertedDistance);
 }
